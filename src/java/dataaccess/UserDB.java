61c249a7dc179ed99aeee4da3e9378fcb527c046
727153 <727153@ELF6215730.ACDM.DS.SAIT.CA>
2017-10-20 14:11:12 -0600

    Add import ResultSet

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -4,6 +4,7 @@ import domainmodel.User;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
