6a484175d22ac2836662a63ae86a26cfe0d06518
Shashank Patel <31839002+shashank-patel@users.noreply.github.com>
2017-10-20 14:13:20 -0600

    Delete and GetUserById
    Both works fine

--- src/java/businesslogic/UserService.java
+++ src/java/businesslogic/UserService.java
@@ -1,16 +1,46 @@
 package businesslogic;
 
+import dataaccess.NotesDBException;
 import domainmodel.User;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
-
+    
     public User get(String username) throws Exception {
         return null;
     }
 
-    public List<User> getAll() throws Exception {
-        return null;
+    public List<User> getAll() throws NotesDBException, SQLException {
+        List<User> userList = null;
+        
+        int id;
+        String firstname;
+        String lastname;
+        String email;
+        String password;
+        String sin;
+        
+        String preparedSql="SELECT * FROM Users";
+        PreparedStatement ps =connection.prepareStatement(preparedSql);
+        
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()){
+           
+            id=Integer.parseInt(rs.getString("id"));
+            firstname=rs.getString("firstname");
+            lastname=rs.getString("lastname");
+            email=rs.getString("email");
+            password=rs.getString("password");
+            sin=rs.getString("SIN");
+            User u = new User(id,firstname,lastname,email,password,sin);
+           
+            userList.add(u);
+        }
+        
+        return userList;
     }
 
     public int update(String username, String password, ... [more parameters]) throws Exception {
--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -2,7 +2,6 @@ package dataaccess;
 
 import domainmodel.User;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,25 +13,61 @@ import java.util.logging.Logger;
 public class UserDB {
 
     public int insert(User user) throws NotesDBException, SQLException {
+
        ConnectionPool cp= ConnectionPool.getInstance();
-       Connection connection = cp.getConnection();
-        String query = "UPDATE Users SET " +
-        "id = '" + user.getId() + "', " +
-        "firstname = '" + user.getFirstname()+ "', " +
-         "lastname = '" + user.getLastname()+ "', " + 
-                "email = '" + user.getEmail()+ "', " +
-                 "password = '" + user.getPassword()+ "', " +
-                 "SIN = '" + user.getSIN()+ "', " ;
-               
+       Connection connection = cp.getConnection();        
+        String query = "INSERT INTO Users (id,firstname,lastname,email,password,SIN) " +
+                    "VALUES ('" + user.getId() + "', " +
+                    "'" + user.getFirstname() + "', " +
+                    "'" + user.getLastname() + "', " +
+                    "'" + user.getEmail()+ "', " +
+                    "'" + user.getPassword() + "', " +
+                    "'" + user.getSIN() + "')";
+
         Statement statement = connection.createStatement();
-        
+
         int rowCount = statement.executeUpdate(query);
         cp.freeConnection(connection);
         return rowCount;
     }
 
     public int update(User user) throws NotesDBException {
-        return 0;
+
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        String preparedSQL = "UPDATE USERS SET "
+                + "firstname=?,"
+                + "lastname=?,"
+                + "email=?,"
+                + "password=?,"
+                + "SIN=?"
+                + "WHERE id=?";
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(preparedSQL);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            ps.setString(1, user.getFirstname());
+            ps.setString(2, user.getLastname());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getPassword());
+            ps.setInt(5, user.getSIN());
+            ps.setInt(6, user.getId());
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        int rows = -1;
+        try {
+            rows = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        pool.freeConnection(connection);
+
+        return rows;
+
     }
 
     public List<User> getAll() throws NotesDBException {
@@ -55,8 +90,7 @@ public class UserDB {
         user = (User) user1;
         return user;
     }
-
-    public int delete(User user) throws NotesDBException, SQLException {
+   public int delete(User user) throws NotesDBException, SQLException {
         ConnectionPool cp= ConnectionPool.getInstance();
        Connection connection = cp.getConnection();
         String deleteQuery = "DELETE FROM USERS "
